c2e16cb9bb40045ff3042208977ba61ce6d9993b
JlO2013 <75309873+JlO2013@users.noreply.github.com>
2023-06-16 15:12:55 +0300

    Add files via upload

---
+++ server/Controllers/EmployeesController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using The_Factory.Models;
+
+namespace TheFactory.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class EmployeesController : ApiController
+    {
+        private EmployeesBL employeesBL = new EmployeesBL();
+        private EmployesShiftsBL EmployesShiftsBL = new EmployesShiftsBL();
+        // GET: api/Employees
+        public IEnumerable<EmpWithDeptWithShift> Get()
+        {
+            return employeesBL.GetEmplyees();
+        }
+
+        // GET: api/Employees/5
+        public EmpWithDeptWithShift Get(int id)
+        {
+            return employeesBL.GetEmployee(id);
+        }
+
+        public IEnumerable<EmpWithDeptWithShift> Get(string name)
+        {
+            return employeesBL.GetEmployeeByFirst_Name(name);
+        }
+        public IEnumerable<EmpWithDeptWithShift> GetDept(int departmentID)
+        {
+            return employeesBL.GetEmployeeByDept(departmentID);
+        }
+
+        // POST: api/Employees
+        public string Post(Employee empl)
+        {
+            employeesBL.AddEmployee(empl);
+            return "Created with ID: " + empl.ID;
+
+        }
+
+        // PUT: api/Employees/5
+        public string Put(int id, Employee employee)
+        {
+            employeesBL.UpdateEmployee(id, employee);
+            return " details had been updated!";
+        }
+
+        // DELETE: api/Employees/5
+        public string Delete(int ID)
+        {
+            EmployesShiftsBL.DeleteShift(ID);
+
+            employeesBL.DeleteEmployee(ID);
+            return "Employee had been deleted and shifts had been updated!";
+        }
+
+    }
+}
---
+++ server/Controllers/ShiftsController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using The_Factory.Models;
+using TheFactory.Models;
+
+namespace TheFactory.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ShiftsController : ApiController
+    {
+        private ShiftsBL shiftsBL = new ShiftsBL();
+        private EmployesShiftsBL employesShiftsBL = new EmployesShiftsBL();
+        // GET: api/Shifts
+        public IEnumerable<Shifts> Get()
+        {
+            return shiftsBL.GetShifts();
+        }
+
+        // GET: api/Shifts/5
+        public Shifts Get(int id)
+        {
+            return shiftsBL.GetShift(id);
+        }
+        public List<Shifts4Emps> GetShiftsByEmp(int empsID)
+        {
+            return shiftsBL.GetShiftsByEmp(empsID);
+        }
+
+        // POST: api/Shifts
+        public string Post(Shift shift)
+        {
+            shiftsBL.AddShift(shift);
+            return "New shift created!";
+
+        }
+
+        // PUT: api/Shifts/5
+        public string Put(int ID, EmployeesShift shift)
+        {
+            employesShiftsBL.AddShiftToEmp(shift);
+            employesShiftsBL.UpdateShift(shift.ID, shift);
+
+            return "Shift had been updated!";
+        }
+
+        // DELETE: api/Shifts/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
